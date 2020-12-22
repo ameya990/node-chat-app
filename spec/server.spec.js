9c49fda8ba40ec8fc3fabe25c5fbb05f1e6ea4a3
Ameya <ameya.fall2015@gmail.com>
2020-12-22 01:45:11 +0100

    Adding some async Jasmint test specs

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -1,4 +1,4 @@
-const { request } = require("express");
+const request = require('request');
 
 describe('calc', () => {
     it('should multiply 2 and 2', () => {
@@ -7,9 +7,32 @@ describe('calc', () => {
 })
 
 describe('get messages', () => {
-    it('should return 200 OK', () => {
+    it('should return 200 OK', (done) => {
         request.get('http://localhost:3000/messages', (err, res) => {
-            console.log(res);
+            expect(res.statusCode).toEqual(200);
+            done();
+        })
+    })
+    it('should return return a non empty list', (done) => {
+        request.get('http://localhost:3000/messages', (err, res) => {
+            expect(JSON.parse(res.body).length).toBeGreaterThan(0);
+            done();
+        })
+    })
+})
+
+describe('get messages from user', () => {
+    it('should return 200 OK', (done) => {
+        request.get('http://localhost:3000/messages/tim', (err, res) => {
+            expect(res.statusCode).toEqual(200);
+            done();
+        })
+    })
+    
+    it('name should be tim', (done) => {
+        request.get('http://localhost:3000/messages/tim', (err, res) => {
+            expect(JSON.parse(res.body)[0].name).toEqual('tim');
+            done();
         })
     })
 })
